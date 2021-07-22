a6046c9cbf56c2bfae15bf706ab5cddab92815a2
Adam Nix <36577305+AdamHNix@users.noreply.github.com>
2021-07-22 21:48:20 +0000

    main changes. Push new version to git.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 
-const imdb = require('imdb-api');
-const server = require("express")();
-const session = require("express-session");
+const imdb = require('imdb-api');//imdb lookup API
+const server = require("express")();//server for twilio messages
+const session = require("express-session");//server for twilio messages
 const bodyParser = require('body-parser');
-const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const MessagingResponse = require("twilio").twiml.MessagingResponse;//include twiml
+const from = process.env.PHONE_NUMBER;//twilio phone number
+const msSID =process.env.MESSAGE_SERVICE_SID;//messaging services SID
+const to = process.env.MY_NUMBER;//personal phone for testing
+const movapi = process.env.MOVIE_KEY;//omdb API key
+const port = process.env.EXPRESS_PORT;
+const twilio = require("twilio")(
+  process.env.TOKEN_SID,
+  process.env.TOKEN_SECRET,
+  {
+    accountSid: process.env.ACCOUNT_SID,
+  }
+);
 
-const port = process.env.EXPRESS_PORT || 8080;
+var savedBody = []; //used to save movie responses between texts
+var overflowMessage;
+var phoneNum;// list the from number from the first request
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(session(
@@ -20,19 +34,19 @@ server.listen(port, () => {
   console.log("listening...");
 });
 
-server.get('/test', (request, response) => {
-  response.send("hi!!!!");
-})
-
+//test server. Not needed unless issues arise
+//server.get('/test', (request, response) => {
+//  response.send("hi!!!!");
+//})
+//function takes inputed movie and returns object with imdb information in an array
 async function findMovie(mov, object) {
   return imdb.get(
     { name: mov },
-    { apiKey: '9ed698aa', timeout: 30000 })
+    { apiKey: movapi, timeout: 30000 })
     .then((stats) => {
-      console.log("Stats: ", stats);
       return stats;
     })
-    .catch(console.log("catch stat error"))
+    .catch("error!!!")
     .then((things) => {
       if (things.type == "movie") {
         object[0] = (things.title);
@@ -40,6 +54,14 @@ async function findMovie(mov, object) {
         object[2] = (things.year);
         object[3] = (things.rating);
         object[4] = things.type;
+        object[5] = things.rated;
+        object[6] = things.runtime;
+        object[7] = things.genres;
+        object[8] = things.director;
+        object[9] = things.writer;
+        object[10] = things.actors;
+        object[11] = things.plot;
+        object[12] = things.imdburl;
       }
       else if (things.type == "series") {
         object[0] = (things.title);
@@ -50,65 +72,94 @@ async function findMovie(mov, object) {
         object[5] = things.plot;
         object[6] = things.actors;
         object[7] = things.imdburl;
-      }
-      else {
-
-        return object = [];
+        object[8] = things.totalseasons;
+        object[9] = things.endyear
+        object[10] = things.genres;
+        object[11] = things.director;
+        object[12] = things.imdburl;
+        object[13] = things.writers;
       }
       return object;
     }
     )
-    .catch(console.log("catch error"))
-};
-
+    .catch(console.log("catch error"))};
+//create server for posting  responses from OMDB API
 server.post('/get-sms', (request, response) => {
-  console.log("Test");
-  const body = request.body.Body;
-  const savedBody = body;
-  var movarr = [];
-  const state = request.session.step;
-  (findMovie(body, movarr))
-    .then(res => {
-      console.log("res: ", res);
-      request.session.step = 1;
-      if (res[4] == "movie") {
-        message = ("Looks like you want some info about the movie " + res[0] + ". Here you go! \n\n"
-          + res[0] + " released in " + res[2] + ".\n\nAwards: " + res[1] + "\n\nimdb currently scores it at " + res[3] + "/10.");
-      } else if (res[4] == "series") {
-        message = ("Looks like you want some info about the series " + res[0] + ". Here you go!\n "
-          + res[0] + " released in " + res[2] + ".\n\n Awards: " + res[1] + "\n\nimdb currently scores it at " + res[3] + "/10.");
+  const body = request.body.Body; //create variable for text from user
+  var movarr = []; //used to move array from promise
+  const state = request.session.step; //track responses
+  //this if statement is needed when someone asks for multiple movies in one session. savedBody var will keep the old movie as well, so shifting the first item on the array off the savedBody will ensure only one item is in the array. Probably don't even need an array for this var, but don't want to deal with changing it.
+  if (state == 1 && body == "Y" && from == phoneNum){ 
+    if (savedBody.length != 1){
+      savedBody.shift();
+    }
+    (findMovie(savedBody, movarr)).catch(console.log("catch error"))
+    .then(res =>{
+        request.session.step = 2; //notify that they have already gotten two responses
+        if(res[4] == "movie"){
+                  message = ("Ok! \n\n" + res[0] + " is a " + res[7] + " movie with a runtime of " + res[6] + ".\n\nDirected by " + res[8] + " and written by " + res[9] + ".\n\n Starring " + res[10] + ".\n\nplot: " + res[11]);
+        }
+        else if (res[4] == "series"){
+          message = ("Ok! \n\n" + res[0] + " is a " + res[10] + " series that aired from " + res[2] + " to " + res[9] + ", with a total of " + res[8] + " seasons. It was written by " + res[13] + " and stars " + res[6] + ".\n\nplot: " + res[5]);
+        }
+        if (message.length > 1600){
+        var overflowLength = message.length - 1600;
+        overflowMessage = message.slice (1600-(overflowLength));
+        message = message.slice (0, 1500-overflowLength);
+        message = message + "... It looks like this message is too long." 
       }
-      else if (res[4] != ("movie" || "series")) {
-        message = ("Looks like you want some info about " + res[0] + ". Here you go!\n\n "
-          + res[0] + " released in " + res[2] + ". \n\nAwards: " + res[1] + " \n\nimdb currently scores it at " + res[3] + "/10.");
-      }
-      else {
-        message = "movie or series not found.. Please try again";
-      }
-      console.log("movarr", movarr);
-      //var movieText = JSON.stringify(movieRequest(body));
-      //console.log("movie text:" + movieText);
-      //parsedMovieText = JSON.parse(movieText);
-
-      //console.log("parsed movie text: " + parsedMovieText);
+      message = message + " See " + res[12] + " for more info.";
+      //send to user via Twilio. Could probably turn this into function?
       const twiml = new MessagingResponse();
       twiml.message(message);
       console.log("response: ", twiml.toString());
       response.set("Content-Type", 'text/xml');
       response.send(twiml.toString());
+
+      // if(overflowMessage.length > 0){
+      //   console.log("made it to overflow if");
+      //   const twiml = new MessagingResponse();
+      //   twiml.message(overflowMessage);
+      //   console.log("response: ", twiml.toString());
+      //   response.set("Content-Type", 'text/xml');
+      //   response.send(twiml.toString());        
+      // }
       return res;
-    });
-});
-
-
-const from = process.env.PHONE_NUMBER;
-
-const to = process.env.MY_NUMBER;
-
-const twilio = require("twilio")(
-  process.env.TOKEN_SID,
-  process.env.TOKEN_SECRET,
-  {
-    accountSid: process.env.ACCOUNT_SID,
+    })
   }
-);
+  else
+  {
+    console.log("in state 0");
+    (findMovie(body, movarr))
+    .catch(err => {
+      const twiml = new MessagingResponse();
+      twiml.message("Sorry, I couldn't find any movie or series with that name.. Please check for typos or be a little more specific.");
+      console.log("response: ", twiml.toString());
+      response.set("Content-Type", 'text/xml');
+      response.send(twiml.toString())}
+    )
+    .then(res => {
+      phoneNum = from;
+      if (res[4] == "movie") {
+        request.session.step = 1;
+        savedBody = [];
+        savedBody.push(res[0]);//savedBody does not need to be an array
+        message = ('Looks like you want some info about the movie "' + res[0] + '." Here you go! \n\n'
+          + res[0] + " released in " + res[2] + ".\n\nAwards: " + res[1] + "\n\nimdb currently scores it at " + res[3] + '/10.\n\nWant to learn more? Reply "Y"');
+      } else if (res[4] == "series") {
+        request.session.step = 1;
+        savedBody = [];
+        savedBody.push(res[0]);
+        message = ('Looks like you want some info about the series "' + res[0] + '." Here you go!\n\n '
+          + res[0] + " released in " + res[2] + ".\n\n Awards: " + res[1] + "\n\nimdb currently scores it at " + res[3] + '/10. Reply "Y" for more info.');
+      }
+
+      const twiml = new MessagingResponse();
+      twiml.message(message);
+      console.log("response: ", twiml.toString());
+      response.set("Content-Type", 'text/xml');
+      response.send(twiml.toString());
+
+      return res;
+    })}
+  });
